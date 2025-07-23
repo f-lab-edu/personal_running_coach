c73723857323093ce969f2cf1e3c16b352e89f4e
Jay Kim <jaykim.coding@gmail.com>
2025-07-24 01:43:13 +0900

    db session 구현

---
+++ src/infra/db/storage/session.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from src.config.settings import db
+from sqlmodel import SQLModel
+
+engine = create_async_engine(
+    url=db.url,
+    echo=db.echo,
+)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+async def create_db_and_tables() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def close_db() -> None:
+    await engine.dispose()
+
+# Dependency for FastAPI
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
