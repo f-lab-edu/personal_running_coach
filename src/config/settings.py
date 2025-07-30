598c7db499a1fb71941119bb854316f2a62484fb
Jay Kim <jaykim.coding@gmail.com>
2025-07-31 03:24:43 +0900

    config 세팅 추가

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,23 +1,40 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
-class DatabaseConfig(BaseSettings):
+
+class CommonConfig(BaseSettings):
+    class Config:
+        env_file = ".env"
+        extra = "ignore" 
+
+class JWTConfig(CommonConfig):
+    secret:str = Field(default="secret", alias="JWT_SECRET")
+    algorithm:str = Field(default="HS256", alias="JWT_ALGORITHM")
+
+
+class DatabaseConfig(CommonConfig):
     url: str = Field(default="sqlite+aiosqlite:///./db.sqlite3", alias="DATABASE_URL")
     echo: bool = Field(default=True, alias="DATABASE_ECHO")
 
-
-class CORSConfig(BaseSettings):
+class CORSConfig(CommonConfig):
     origins: str = Field(default="*", alias="CORS_ORIGINS")
     credentials: bool = Field(default=True, alias="CORS_CREDENTIALS")
     methods: str = Field(default="*", alias="CORS_METHODS")
     headers: str = Field(default="*", alias="CORS_HEADERS")
 
-
-class WebConfig(BaseSettings):
+class WebConfig(CommonConfig):
     host: str = Field(default="0.0.0.0", alias="WEB_HOST")
     port: int = Field(default=8000, alias="WEB_PORT")
+    
+class GoogleConfig(CommonConfig):
+    client_id: str = Field(default="", alias="GOOGLE_CLIENT_ID")
+    client_secret: str = Field(default="", alias="GOOGLE_CLIENT_SECRET")
+    redirect_uri: str = Field(default="", alias="GOOGLE_REDIRECT_URI")
+    scope: str = Field(default="", alias="GOOGLE_SCOPE")
+    auth_endpoint: str = Field(default="", alias="GOOGLE_AUTH_ENDPOINT")
 
 
 db = DatabaseConfig()
 cors = CORSConfig()
 web = WebConfig()
+google = GoogleConfig()
